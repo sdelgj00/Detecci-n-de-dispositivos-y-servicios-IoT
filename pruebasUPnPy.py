b932d787a2f5b0c18c56b0ee594a660840d72bd4
sdelgj00 <47608545+sdelgj00@users.noreply.github.com>
2021-10-28 18:34:38 +0200

    Más modificaciones para claridad

--- pruebasUPnPy.py
+++ pruebasUPnPy.py
@@ -4,24 +4,25 @@ if __name__== "__main__":
     #Añadimos el UPnP al xml
     UPnPXml=etree.Element("UPnP")
     upnp=upnpy.UPnP()
-    dispositivos=upnp.discover()
+    devices=upnp.discover()
     #añadimos los dispositivos al xml
-    for dispositivo in dispositivos:
-        nombreDispositivo=str(dispositivo)
-        nombreDispositivo=nombreDispositivo.replace("Device <","")
-        nombreDispositivo=nombreDispositivo.replace(">","")
-        dispositivoXml=etree.SubElement(UPnPXml,nombreDispositivo, IP=dispositivo.address[0], port=str(dispositivo.address[1]))
-        serviciosXml=etree.SubElement(dispositivoXml,"services")
+    for device in devices:
+        deviceName=str(device)
+        deviceName=deviceName.replace("Device <","")
+        deviceName=deviceName.replace(">","")
+        deviceXml=etree.SubElement(UPnPXml,deviceName, IP=device.address[0], port=str(device.address[1]))
+        servicesXml=etree.SubElement(deviceXml,"services")
         #añadimos los servicios de cada dispositivo al xml
-        for servicio in dispositivo.get_services():
-            servicioTXT=str(servicio)
-            servicioDividido=servicioTXT.split()
-            servicioXml=etree.SubElement(serviciosXml,servicioDividido[1].replace("(","").replace(")",""),
-            ID=servicioDividido[2].replace("id=","").replace('"',"").replace(">",""),
-            SCPD=servicio.scpd_url, ControlUrl=servicio.control_url, EventUrl=servicio.event_sub_url,
-            BaseUrl=servicio.base_url)
-            actions=servicio.get_actions()
-            actionsXml=etree.SubElement(servicioXml, "actions")
+        for service in device.get_services():
+            #el proceso de hacer el split es para obtener el nombre y el id del servicio
+            serviceTXT=str(service)
+            dividedService=serviceTXT.split()
+            serviceXml=etree.SubElement(servicesXml,dividedService[1].replace("(","").replace(")",""),
+            ID=dividedService[2].replace("id=","").replace('"',"").replace(">",""),
+            SCPD=service.scpd_url, ControlUrl=service.control_url, EventUrl=service.event_sub_url,
+            BaseUrl=service.base_url)
+            actions=service.get_actions()
+            actionsXml=etree.SubElement(serviceXml, "actions")
             #añadimos las acciones de cada servicio
             for action in actions:
                 actionXml=etree.SubElement(actionsXml,action.name)
@@ -30,9 +31,6 @@ if __name__== "__main__":
                 #añadimos los argumentos de entrada a la acción
                 inputArgsXml=etree.SubElement(actionXml, "input_args")
                 for inputArg in inputArgs:
-                    print(inputArg['name'])
-                    print(inputArg['data_type'])
-                    print(inputArg['allowed_value_list'])
                     allowedVals=inputArg['allowed_value_list']
                     allowedValsString=""
                     for vals in allowedVals:
@@ -43,9 +41,6 @@ if __name__== "__main__":
                 #añadimos los argumentos de salida a la acción
                 outputArgsXml=etree.SubElement(actionXml, "output_args")
                 for outputArg in outputArgs:
-                    print(outputArg['name'])
-                    print(outputArg['data_type'])
-                    print(outputArg['allowed_value_list'])
                     allowedVals=outputArg['allowed_value_list']
                     allowedValsString=""
                     for vals in allowedVals:
@@ -53,9 +48,6 @@ if __name__== "__main__":
                     allowedValsString=allowedValsString[1:len(allowedValsString)]
                     outputArgXml=etree.SubElement(outputArgsXml,outputArg['name'], dataType=outputArg['data_type'],
                     allowedValueList=allowedValsString)
-                print(action.get_input_arguments())
-                print(action.get_output_arguments())
-                print(action.name)
     #escribimos en el archivo ./xmls/UPnP.xml el xml correspondiente al escaneo mediante el protocolo UPnP
     archivoXml=etree.ElementTree(UPnPXml)
     archivoXml.write("./xmls/UPnP.xml")
