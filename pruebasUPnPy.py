4607c38aee0b301745ef55aff88f202d2a55106f
sdelgj00 <47608545+sdelgj00@users.noreply.github.com>
2021-10-28 18:17:27 +0200

    Retoques en el código para que sea más legible

--- pruebasUPnPy.py
+++ pruebasUPnPy.py
@@ -5,14 +5,6 @@ if __name__== "__main__":
     UPnPXml=etree.Element("UPnP")
     upnp=upnpy.UPnP()
     dispositivos=upnp.discover()
-    print(dispositivos[0].address)
-    print((dispositivos[0].response))
-    print(dispositivos[0])
-    #print(dispositivos[0].get_services()[1].service_id)
-    print(dispositivos[0].get_services())
-    print("hola")
-    print(dispositivos[0].get_services()[0])
-    print(dispositivos[0].get_services()[0].base_url)
     #añadimos los dispositivos al xml
     for dispositivo in dispositivos:
         nombreDispositivo=str(dispositivo)
@@ -29,15 +21,14 @@ if __name__== "__main__":
             SCPD=servicio.scpd_url, ControlUrl=servicio.control_url, EventUrl=servicio.event_sub_url,
             BaseUrl=servicio.base_url)
             actions=servicio.get_actions()
-            print("acciones")
-            print(actions)
             actionsXml=etree.SubElement(servicioXml, "actions")
             #añadimos las acciones de cada servicio
             for action in actions:
                 actionXml=etree.SubElement(actionsXml,action.name)
-                inputArgsXml=etree.SubElement(actionXml, "input_args")
                 inputArgs=action.get_input_arguments()
                 outputArgs=action.get_output_arguments()
+                #añadimos los argumentos de entrada a la acción
+                inputArgsXml=etree.SubElement(actionXml, "input_args")
                 for inputArg in inputArgs:
                     print(inputArg['name'])
                     print(inputArg['data_type'])
@@ -49,6 +40,7 @@ if __name__== "__main__":
                     allowedValsString=allowedValsString[1:len(allowedValsString)]
                     inputArgXml=etree.SubElement(inputArgsXml,inputArg['name'], dataType=inputArg['data_type'],
                     allowedValueList=allowedValsString)
+                #añadimos los argumentos de salida a la acción
                 outputArgsXml=etree.SubElement(actionXml, "output_args")
                 for outputArg in outputArgs:
                     print(outputArg['name'])
@@ -64,17 +56,9 @@ if __name__== "__main__":
                 print(action.get_input_arguments())
                 print(action.get_output_arguments())
                 print(action.name)
+    #escribimos en el archivo ./xmls/UPnP.xml el xml correspondiente al escaneo mediante el protocolo UPnP
     archivoXml=etree.ElementTree(UPnPXml)
     archivoXml.write("./xmls/UPnP.xml")
-    """IGD=dispositivos[0]
-    print(IGD)
-    print(IGD.get_services())
-    servicio=IGD['WANIPConn1']
-    print(servicio)
-    print(servicio.get_actions())
-    print(servicio.GetExternalIPAddress())
-    print(servicio.GetStatusInfo.get_input_arguments())
-    print(servicio.GetStatusInfo.get_output_arguments())
-    print(servicio.GetStatusInfo())"""
+   
 
 
